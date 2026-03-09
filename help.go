charmbracelet/fang

commit f227d7e6f5bf90bebbf95d251be7f3137872649c

fix: lint: preallocate the groups slice in evalGroups

help.go
@@ -434,7 +434,7 @@ func evalCmds(c *cobra.Command, styles Styles) (map[string](map[string]string), 
 
 func evalGroups(c *cobra.Command) (map[string]string, []string) {
 	// make sure the default group is the first
-	ids := []string{""}
+	ids := make([]string, 0, len(c.Groups())+1)
 	groups := map[string]string{"": "commands"}
 	for _, g := range c.Groups() {
 		groups[g.ID] = g.Title

commit 450f7dbb3c45e663dad94a9882edd47c4e40874c

fix: properly preallocate the groups slice in evalGroups
Fixes: f227d7e6f5bf (fix: lint: preallocate the groups slice in evalGroups)

help.go
@@ -434,7 +434,7 @@ func evalCmds(c *cobra.Command, styles Styles) (map[string](map[string]string), 
 
 func evalGroups(c *cobra.Command) (map[string]string, []string) {
 	// make sure the default group is the first
-	ids := make([]string, 0, len(c.Groups())+1)
+	ids := make([]string, len(c.Groups())+1)
 	groups := map[string]string{"": "commands"}
 	for _, g := range c.Groups() {
 		groups[g.ID] = g.Title